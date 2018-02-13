2f873e03239d43dc908298709ff9c9e0e7f9f5d2
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2018-02-13 12:15:06 +0530

    Create AreaDTO.java

---
+++ com/erp/dto/AreaDTO.java
@@ -0,0 +1,37 @@
+package com.erp.dto;
+
+
+
+public class AreaDTO {
+	private int areaId;
+	private String regName;
+	private String areaName;
+	private String description;
+	
+	public int getAreaId() {
+		return areaId;
+	}
+	public void setAreaId(int areaId) {
+		this.areaId = areaId;
+	}
+	public String getRegName() {
+		return regName;
+	}
+	public void setRegName(String regName) {
+		this.regName = regName;
+	}
+	public String getAreaName() {
+		return areaName;
+	}
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
